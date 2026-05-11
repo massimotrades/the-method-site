a0a5756e9fec2298835a7b3428299ad351d4a118
massimotrades <massimodeangelis8@gmail.com>
2026-05-11 19:51:20 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React, { useMemo, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import { motion } from "framer-motion";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TheMethodEliteWebsite />
+  </React.StrictMode>
+);
